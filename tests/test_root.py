179183917b8a22ff8fe4e17b051d7cfef90ec3fa
Stefodan21 <stefaandavis21@outlook.com>
2025-07-10 22:54:01 -0400

    Updated the test to check for the message in the root endpoint response.

--- tests/test_root.py
+++ tests/test_root.py
@@ -4,5 +4,5 @@ def test_root_alive():
     client = api.test_client()
     res = client.get("/")
     assert res.status_code == 200
-    assert "Order Fulfillment API is running" in res.get_data(as_text=True)
+    assert "Order Fulfillment API is Running!" in res.get_data(as_text=True)
 
